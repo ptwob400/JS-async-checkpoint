7924ca3c82662e84ca1ced0f4462acd8615bd94f
ptwob400 <ptwob400@gmail.com>
2021-03-04 22:44:28 -0600

    hardcoded but works

--- README.txt
+++ README.txt
@@ -1,1 +1,5 @@
 This is the checkpoint for async @gschool.
+
+Instructions:
+1. list out any pokemon in the pokemonlist.txt (one pokemon per line)
+2. Save the file and then run node . (the command 'node .' ) in the correct directory to get type for each pokemon!
--- index.js
+++ index.js
@@ -1,47 +1,25 @@
-#!/mnt/c/users/richie/desktop/richie/sdi/git/async-checkpoin/en node
-
-const yargs = require("yargs");
-const axios = require("axios");
 const fetch = require('node-fetch');
-
 var fs = require('fs');
 var request = require('request');
 
-const options = yargs
- .usage("Usage: -n <name>")
- .option("n", { alias: "name", describe: "Your name", type: "string", demandOption: true })
- .argv;
+const fileContents = fs.readFileSync('pokemonlist.txt', 'utf8')
+const pokemonArray = fileContents.split('\n');
+var sizeOfArray = pokemonArray.length
 
- /* var pokemonArray = function (filepath, callback) {
-     const fileContents = fs.readFile(filePath, 'utf8', function (err, content){
-        if (err) {
-          callback(err)
-        } else {
-          const pokemon = content.split("\n")
-          callback(err, pokmeon)
-        }    
-      });
- }; */
-
- pokemonArray = function () {
-     (__dirname + 'pokemonlist.txt', function(content) {
-    const pokemon = content.split("\n");
-    return pokemon;
- });
-};
-
- var url = "https://pokeapi.co/api/v2/pokemon/" + pokemonArray[1];
+for (let i = 0; i < sizeOfArray; i++) {
+ var url = "https://pokeapi.co/api/v2/pokemon/" + pokemonArray[i];
  
  fetch(url)
-  .then(function(data){
-    console.log(data)
-  });
-
-/* for (let i = 0; i < pokemonArray.length; i++) {
-  var url = "https://pokeapi.co/api/v2/pokemon/" + pokemonArray[i];
-
- axios.get(url, { headers: { Accept: "application/json" } })
- .then(res => {
-   console.log(res.data.joke);
- }); 
-} */
+ .then(function(response) {
+    return response.json()
+  })
+ .then(function(data){
+     if (data.types.length === 3) {
+         console.log(`${pokemonArray[i]}: ${data.types[0].type.name}, ${data.types[1].type.name}, ${data.types[2].type.name}`);
+     } else if (data.types.length === 2) {
+         console.log(`${pokemonArray[i]}: ${data.types[0].type.name}, ${data.types[1].type.name}`);
+     } else {
+      console.log(`${pokemonArray[i]}: ${data.types[0].type.name}`);
+     }
+   })
+ }
